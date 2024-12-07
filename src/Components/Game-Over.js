d2a35bccf1737be9bf78e02fc702c3e26abbb0f8
Kamlesh <136187579+DEV007777@users.noreply.github.com>
2024-12-07 19:56:02 +0530

    Update Game-Over.js

--- src/Components/Game-Over.js
+++ src/Components/Game-Over.js
@@ -1,7 +1,7 @@
 export default function GameOver({ winner,aftermatch }) {
   return (
     <div id="game-over">
-        <h2>Game Over!!</h2>
+        <h2>Game Over!!!</h2>
      {winner && <p> {winner} Won!</p>}
      {!winner && <p> It's DRAW!!</p>}
       <p>
